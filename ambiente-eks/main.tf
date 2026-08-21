fabcc0a8145fa3f5b9ad9ce48f4fc905c081dfe7
Rafael Monteiro <rafael.monteiro@pro4tech.com.br>
2026-08-20 21:38:37 -0300

    Ajuste de formatação

--- ambiente-eks/main.tf
+++ ambiente-eks/main.tf
@@ -1,4 +1,4 @@
 module "nosso_eks" {
-  source      = "../Module/eks" # Chamando o módulo
-  cluster_name= "nosso_eks"
+  source       = "../Module/eks" # Chamando o módulo
+  cluster_name = "nosso_eks"
 }
